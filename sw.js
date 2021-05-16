09a59e8712ff5ea0dea4a4f178adba2d80ef09a8
tmukesh2809 <68456993+tmukesh2809@users.noreply.github.com>
2021-05-15 20:30:39 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,3 +4,66 @@
 //   - One for installation
 //   - One for activation ( check out MDN's clients.claim() for this step )
 //   - One for fetch requests
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  'https://cse110lab6.herokuapp.com/entries'
+];
+
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function(cache) {
+          console.log('Opened cache');
+          return cache.addAll(urlsToCache);
+        })
+    );
+  });
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(clients.claim()); 
+    var cacheAllowlist = ['my-site-cache-v1'];
+  
+    event.waitUntil(
+      
+      caches.keys().then(function(cacheNames) {
+        // self.clients.claim();
+        return Promise.all(
+          cacheNames.map(function(cacheName) {
+            if (cacheAllowlist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+          return fetch(event.request).then(
+            function(response) {
+              // Check if we received a valid response
+              if(!response || response.status !== 200 || response.type !== 'basic') {
+                return response;
+              }
+  
+              var responseToCache = response.clone();
+  
+              caches.open(CACHE_NAME)
+                .then(function(cache) {
+                  cache.put(event.request, responseToCache);
+                });
+  
+              return response;
+            }
+          );
+        })
+      );
+  });
