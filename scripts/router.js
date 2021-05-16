c625b8ef6d83d172371b3ca8cd2bfaa92f1ab8e0
tmukesh2809 <68456993+tmukesh2809@users.noreply.github.com>
2021-05-16 14:31:53 -0700

    Update router.js

--- scripts/router.js
+++ scripts/router.js
@@ -5,73 +5,41 @@ export const router = {};
 /**
  * Changes the "page" (state) that your SPA app is currently set to
  */
-router.setState = function(header, hashurl, className) {
-  /**
-   * - There are three states that your SPA app will have
-   *    1. The home page
-   *    2. The entry page (showing one individual entry)
-   *    3. The settings page (currently blank, no actual settings here, just a placeholder where a real settings page would go)
-   * 
-   * - If you look at the CSS, we have 2 classes you can add to the body element to help change states, "settings" and "single-entry"
-   * - Changing states will require more than just changing these classes, for example the settings page requires you to change the title to "Settings"
-   * - And each individual entry the title changes to "Entry #" based on it's number in the entry order
-   *
-   * - When changing states, make sure the back and forward buttons work. You can use hash URLs (e.g. https://someurl.com/#settings) when changing states
-   *   to make things easier.
-   * - Similarly, when viewing an individual entry, a hashed URL might look like https://someurl.com/#entry3
-   * 
-   * - Some tips:
-   *    1. Push a new state object to the history object using history.pushState() 
-   *    2. look up the documentation for how to use pushState() when you try it
-   *    3. look up the documentation for the "popstate" event listener (fires only on back button), useful in your script.js file
-   *    4. For each <journal-entry> element, you can grab the JSON version of its info with .entry (e.g. someJournalEntryElement.entry)
-   *       a. This is useful when viewing a single entry. You may notice an <entry-page> element in the HTML, this is the element that is displayed when the
-   *          .single-entry class is applied to the body. You can populate this element by using .entry similarly. So if I wanted to grab a specific <journal-entry>
-   *          and populate it's info into the <entry-page>, I would simply use an assignment of entryPageElement.entry = journalEntryElement.entry
-   *       b. Clearing the <entry-page> element of its previous data can be a bit tricky, it might be useful to just delete it and insert a new blank one 
-   *          in the same spot each time. Just a thought.
-   *
-   * - Answers to some questions you may have:
-   *    1. You may add as many helper functions in this file as you like
-   *    2. You may modify the parameters of setState() as much as you like
-   */
-  let state = {'header': header, 'hashurl': hashurl, 'className': className};
-  let url = hashurl;
-  history.pushState(state, '', url);
+router.setState = function(titleheader, link, className) {
+  let state = {'header': titleheader, 'link': link, 'className': className};
+  history.pushState(state, '', link);
 
   document.body.className = className;
-  document.querySelector('header h1').innerText = header;
+  document.querySelector('header h1').innerText = titleheader;
 }
 
 window.addEventListener('popstate', (event) => {
+  let defaultTitle = "Journal Entries check Git"
   if (event.state) {
     document.body.className = event.state['className'];
     document.querySelector('header h1').innerText = event.state['header'];
   }
   else {
     document.body.className = "";
-    document.querySelector('header h1').innerText = "Journal Entrieooooooooos";
+    document.querySelector('header h1').innerText = defaultTitle;
   }
 });
 
-// Fires whenever a journal entry is clicked
-const entries = document.querySelectorAll('journal-entry');
 document.addEventListener('click', function(event) {
-  let name = event.target.nodeName.toLowerCase();
-  if (name == "journal-entry") {                                            // journal entry clicked (go to single entry view)
-    let prevEntry = document.querySelector('entry-page');
-    let newEntry = document.createElement('entry-page');
-    newEntry.entry = event.target.entry;
-    document.body.appendChild(newEntry);
-    prevEntry.remove();
-  //  let entryNum = getNodeIndex(event.target) + 1;
-    router.setState("Entry "+5 , "https://hebilli.github.io/Lab7/entry" +'5' , "single-entry");
+  let clickedItem = event.target.nodeName;
+  if (clickedItem == "JOURNAL-ENTRY") {  
+  if(document.querySelector('entry-page')){
+    document.querySelector('entry-page').remove();
+  }                                         // journal entry clicked (go to single entry view)
+   let entryPage = document.createElement('entry-page');
+    entryPage.entry = event.target.entry;
+    document.body.appendChild(entryPage);
+    router.setState("Entry "+679094, "./#entry"+69, "single-entry");
   }
-  else if (event.target.isSameNode(document.querySelector('header img'))) { // settings button clicked (go to settings)
-    router.setState("Settings", "https://hebilli.github.io/Lab7/#settings", "settings");
+  else if (event.target == (document.querySelector('header h1'))) {  // title header clicked (return to main page)
+    router.setState("Journal Entries", "./", "");
   }
-  else if (event.target.isSameNode(document.querySelector('header h1'))) {  // title header clicked (return to main page)
-    router.setState("Journal Entrieuus", "https://hebilli.github.io/Lab7/", "");
+  else if (event.target == (document.querySelector('header img'))) { // settings button clicked (go to settings)
+    router.setState("Settings420", "./#settings", "settings");
   }
 });
-
