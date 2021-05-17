69c87c77877463553fdac83cab4c596fbbe45fae
tmukesh2809 <68456993+tmukesh2809@users.noreply.github.com>
2021-05-16 21:16:14 -0700

    Update router.js

--- scripts/router.js
+++ scripts/router.js
@@ -6,10 +6,11 @@ export const router = {};
  * Changes the "page" (state) that your SPA app is currently set to
  */
 // function to set the states and related properties of each page
-router.setState = function(titleheader, link, className) {
-  let state = {'header': titleheader, 'link': link, 'className': className};
+router.setState = function(titleheader, link, className, entrypg) {
+  let state = {'header': titleheader, 'link': link, 'className': className, 'entrypg': entrypg};
   document.body.className = className;
   document.querySelector('header h1').innerText = titleheader;
   history.pushState(state, 'title', link);
 }
+
 
